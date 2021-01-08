f8eb1303bc15c0bb61a6345e15e648bb09681de7
Hayk <haykmelqonyann@gmail.com>
2021-01-08 18:00:24 +0300

    final html and js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 alert("Git!");
+
+alert("Welcome to The Online Calculator!");
+
+const calFirst = +prompt("Write Your First Number");
+const exType = +prompt("What You Want(1 - + , 2 - - , 3 - * , 4 - /)");
+const calTwo = +prompt("Write Your Second Number");
+
+try {
+    if (exType == 1) {
+        alert(calFirst + calTwo);
+    } else if (exType == 2) {
+        alert(calFirst - calTwo);
+    } else if (exType == 3) {
+        alert(calFirst * calTwo);
+    } else if (exType == 4) {
+        alert(calFirst / calTwo);
+    } else {
+        alert("Error");
+    }
+} catch (err) {
+    alert("What is the Going Wrong , Try Again!");
+}
